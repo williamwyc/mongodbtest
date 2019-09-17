4d245048cd24c9c46f9d399250e45aa29217239f
Yichun Wu <kobo1998@live.com>
2019-09-17 15:09:42 -0400

    Fix a bug

--- app/index.js
+++ app/index.js
@@ -8,7 +8,6 @@ var port = process.env.PORT || 3000;
 app.use(bodyParser.json()); // parse application/json 
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
-app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
 app.use(express.static(__dirname + '/public'));
 
 MongoClient.connect('mongodb://localhost:27017/hw2', function(err, client){
